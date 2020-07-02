260a84da8def598f4dbf72785ceff0bc0dedb2a2
Paul Delafosse <paul.delafosse@protonmail.com>
2020-07-02 18:07:27 +0200

    chore: init

---
+++ src/main/java/com/example/omdbdemo/OmdbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.omdbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdbdemoApplication.class, args);
+	}
+
+}
